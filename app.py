0f3c35ca386ff24a78cf01df4fc7a685e2bf4609
Datehoer <62844803+datehoer@users.noreply.github.com>
2023-02-20 22:14:01 +0800

    go back

--- app.py
+++ app.py
@@ -1,43 +1,23 @@
 from flask import Flask, redirect, render_template, request, url_for
 import openai
-
 app = Flask(__name__)
-# openai.api_key = 'sk-BMstis98onIYjVWhM7LxT3BlbkFJqvxWxCLJyYX8GiLZ2QJihmk'
-
+openai.api_key = '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'
 
 @app.route("/", methods=("GET", "POST"))
 def index():
     if request.method == "POST":
         description = request.form["description"]
         key = request.form["key"]
-        api = request.form["api"]
-        gen_type = request.form["type"]
         if key == "123":
-            if "sk" in api:
-                openai.api_key = api
-            text_model = request.form["text_model"]
-            if gen_type == 'text':
-                response = openai.Completion.create(
-                    model=text_model,
-                    prompt=description,
-                    temperature=0.6,
-                    max_tokens=2048
-                )
-                return redirect(url_for("index", result=response.choices[0].text, gen_type=gen_type))
-            elif gen_type == 'image':
-                img_n = request.form["img_n"]
-                img_size = request.form["img_size"]
-                response = openai.Image.create(
-                    prompt=description,
-                    n=img_n,
-                    size=img_size
-                )
-                print("send ok")
-                return redirect(url_for("index", result=response.data[0].url, gen_type=gen_type))
+            response = openai.Completion.create(
+                model="text-davinci-003",
+                prompt=description,
+                temperature=0.6,
+                max_tokens=2048
+            )
+            return redirect(url_for("index", result=response.choices[0].text))
         else:
-            return redirect(url_for("index", result="Wrong key", gen_type=''))
-    else:
-        return render_template("index.html")
+            return redirect(url_for("index", result="Wrong key"))
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    result = request.args.get("result")
+    return render_template("index.html", result=result)
--- static/main.js
+++ static/main.js
@@ -1,14 +1,6 @@
-console.log(1)
-console.log($("#gen_type").val());
-$("#gen_type").change(function(){
-    console.log($(this).val());
-    if($(this).val() == "image"){
-        $('#img_n').show();
-        $('#img_size').show();
-        $('#text_model').hide();
-    }else{
-        $('#img_n').hide();
-        $('#img_size').hide();
-        $('#text_model').show();
-    }
+$("#result").setTextareaCount({
+    width: "30px",
+    bgColor: "#999",
+    color: "#FFF",
+    display: "block",
 })
